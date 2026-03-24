4f15c8e8f1b4a099db39128da95c19140f45a2d2
Sneha Sunny <56714324+snehhas@users.noreply.github.com>
2026-03-24 22:39:31 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,13 @@
+# Last updated: 24/03/2026, 22:39:31
+class Solution(object):
+    def canJump(self, nums):
+        GOOD, BAD, UNKNOWN = 1, 0, -1
+        memo = [UNKNOWN] * len(nums)
+        memo[-1] = GOOD
+        for i in range(len(nums) - 2, -1, -1):
+            furthest_jump = min(i + nums[i], len(nums) - 1)
+            for j in range(i + 1, furthest_jump + 1):
+                if memo[j] == GOOD:
+                    memo[i] = GOOD
+                    break
+        return memo[0] == GOOD
