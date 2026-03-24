c0a422f10b6b0167597469200ae8e9e5f2f81edd
Sneha Sunny <56714324+snehhas@users.noreply.github.com>
2026-03-24 22:39:04 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.py
@@ -0,0 +1,13 @@
+# Last updated: 24/03/2026, 22:39:04
+class Solution:
+    def candy(self, ratings):
+        candies = [1] * len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        sum = candies[-1]
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+            sum += candies[i]
+        return sum
