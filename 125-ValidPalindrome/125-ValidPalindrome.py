1984445ec0ed8d2179f29ed36c42c9856f3f43f2
Sneha Sunny <56714324+snehhas@users.noreply.github.com>
2026-03-24 22:39:09 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,11 @@
+# Last updated: 24/03/2026, 22:39:09
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        filtered_chars = filter(lambda ch: ch.isalnum(), s)
+        lowercase_filtered_chars = map(lambda ch: ch.lower(), filtered_chars)
+
+        filtered_chars_list = list(lowercase_filtered_chars)
+        reversed_chars_list = filtered_chars_list[::-1]
+
+        return filtered_chars_list == reversed_chars_list
