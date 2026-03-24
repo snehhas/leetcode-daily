459d1356605ca1c576de092a788d1f974f8192a5
Sneha Sunny <56714324+snehhas@users.noreply.github.com>
2026-03-24 22:39:36 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -0,0 +1,20 @@
+# Last updated: 24/03/2026, 22:39:36
+class Solution:
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        ans = 0
+        left_max, right_max = 0, 0
+        while left < right:
+            if height[left] < height[right]:
+                left_max = max(left_max, height[left])
+                ans += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(right_max, height[right])
+                ans += right_max - height[right]
+                right -= 1
+        return ans
